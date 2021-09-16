faaa972c47ceae7834852583f78a0b869f9769bf
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-09-16 12:27:37 +0300

    Create non_reference_parameter.cpp

---
+++ ders_03/non_reference_parameter.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <algorithm>
+
+//iki fonksiyonu karsilastirin
+
+void func(const std::string &s)
+{
+	auto s_copy{ s };
+	reverse(begin(s_copy), end(s_copy));
+	//...
+}
+
+void func(std::string s)
+{
+	reverse(begin(s), end(s));
+	//...
+}
+
+
