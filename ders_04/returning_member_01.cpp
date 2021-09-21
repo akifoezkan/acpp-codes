98048ec604a62c2647a1130359210a3ea3983a83
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-09-21 15:31:27 +0300

    Create returning_member_01.cpp

---
+++ ders_04/returning_member_01.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <iostream>
+
+struct Person {
+	//...
+	const std::string& get_name()const& 
+	{
+		return m_name;
+	}
+
+	std::string get_name()&&
+	{
+		return m_name;
+	}
+
+
+	std::string m_name{ "necati ergin" };
+};
+
+
+Person create_person()
+{
+	Person p;
+	///
+
+	return p;
+}
+
+int main()
+{
+	for (auto c : create_person().get_name())
+		std::cout.put(c);
+}
