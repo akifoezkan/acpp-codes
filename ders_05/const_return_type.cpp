74c5541f861a58a3424610513d27dd2d8511697a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-09-23 17:02:15 +0300

    Create const_return_type.cpp

---
+++ ders_05/const_return_type.cpp
@@ -0,0 +1,37 @@
+//const return type taşıma semantiğini engelliyor.
+
+#include <iostream>
+
+class Nec {
+public:
+	Nec() = default;
+	Nec(const Nec&) = default;
+	Nec(Nec&&) = default;
+	Nec& operator=(const Nec&)
+	{
+		std::cout << "copy assignment\n";
+		return *this;
+	}
+
+	Nec& operator=(Nec&&)
+	{
+		std::cout << "move assignment\n";
+		return *this;
+	}
+};
+
+
+const Nec foo()
+{
+	Nec x;
+
+	return x;
+}
+
+int main()
+{
+	Nec x;
+	//..
+
+	x = foo();
+}
