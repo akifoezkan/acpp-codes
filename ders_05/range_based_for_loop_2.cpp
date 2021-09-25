411379d780e858f6e53f9f697bbed08452c0b846
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-09-25 19:34:02 +0300

    Create range_based_for_loop_2.cpp

---
+++ ders_05/range_based_for_loop_2.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+class Myclass{};
+
+template <typename C, typename T>
+void fill(C& con, const T& val)
+{
+	//for (auto&&& elem : con)
+	for (auto& elem : con)
+		elem = val;
+}
+
+int main()
+{
+	std::vector<int> ivec(20);
+
+	fill(ivec, 10);
+	std::vector<bool> bvec(20);
+	fill(bvec, true);  //gecersiz
+}
