52c544403c3cccf81c7c3972eb398c5f92bc814a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-10-01 07:19:42 +0300

    Create iostream_02.cpp

---
+++ ders_07/iostream_02.cpp
@@ -0,0 +1,26 @@
+#include <string_view>
+#include <fstream>
+#include <stdexcept>
+
+std::ofstream open_write_file(const std::string &filename)
+{
+	std::ofstream ofs{ filename};
+	if (!ofs) {
+		throw std::runtime_error{ "cannot create file!\n" };
+	}
+	return ofs;
+}
+
+void write_file(std::ofstream ofs)
+{
+	ofs << "necati ergin";
+}
+
+int main()
+{
+	auto ofs{ open_write_file("output.txt") };
+	//...
+	write_file(std::move(ofs));
+	///...
+}
+
