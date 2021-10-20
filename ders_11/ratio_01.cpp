f48f37cad641ba70517a97a4e4b32e3fc72491e3
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-10-21 00:28:03 +0300

    Create ratio_01.cpp

---
+++ ders_11/ratio_01.cpp
@@ -0,0 +1,29 @@
+#include <ratio>
+#include <iostream>
+
+template<intmax_t x, intmax_t y>
+std::ostream& operator<<(std::ostream& os, std::ratio<x, y> r)
+{
+	return os << x << " / " << y;
+}
+
+int main()
+{
+	using namespace std;
+	cout << "peta   = " << peta{} << "\n";
+	cout << "exa    = " << exa{} << "\n";
+	cout << "tera   = " << tera{} << "\n";
+	cout << "giga   = " << giga{} << "\n";
+	cout << "mega   = " << mega{} << "\n";
+	cout << "kilo   = " << kilo{} << "\n";
+	cout << "hecto  = " << hecto{} << "\n";
+	cout << "deca   = " << deca{} << "\n";
+	cout << "deci   = " << deci{} << "\n";
+	cout << "centi  = " << centi{} << "\n";
+	cout << "milli  = " << milli{} << "\n";
+	cout << "micro  = " << micro{} << "\n";
+	cout << "nano   = " << nano{} << "\n";
+	cout << "pico   = " << pico{} << "\n";
+	cout << "femto  = " << femto{} << "\n";
+	cout << "atto   = " << atto{} << "\n";
+}
