304235760d577598f1ea7f4041ce1681685898bd
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-10-21 12:32:19 +0300

    Create no_null_character_01.cpp

---
+++ ders_10/no_null_character_01.cpp
@@ -0,0 +1,11 @@
+#include <string_view>
+#include <iostream>
+
+int main()
+{
+	char str[] = { 'a', 'l', 'p', 'e', 'r' };
+	std::string_view sv = str;
+
+	std::cout << sv << "\n"; //ub
+	std::cout << sv.data() << "\n"; //ub
+}
