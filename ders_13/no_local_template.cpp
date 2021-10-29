63ffd304363aebc6882addc4b7b3d13ccbfb79ff
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-10-29 14:21:11 +0300

    Create no_local_template.cpp

---
+++ ders_13/no_local_template.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+//yerel düzeyde şablon oluşturulamaz
+
+
+//gecersiz
+void func()
+{
+	template <typename T>
+	class Myclass {
+
+	};
+}
+
+//gecersiz
+void foo()
+{
+	template <typename T>
+	using Ptr = T*;
+}
