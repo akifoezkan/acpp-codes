c91b5975955dfe8342f470181b7f56d37be23850
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-10-29 11:37:37 +0300

    Create sfinae_01.cpp

---
+++ ders_13/sfinae_01.cpp
@@ -0,0 +1,16 @@
+// #1
+template <typename T>
+typename T::iterator func(T x);
+
+
+// #2
+template <typename T>
+T func(T x);
+
+
+int main()
+{
+	int* p{};
+
+	func(p);  //#1 is sfinaed out  #2 is chosen
+}
