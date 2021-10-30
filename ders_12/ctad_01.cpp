fe1eebf58fe0b3a0f3790b9b016410f13e55a877
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-10-30 11:33:35 +0300

    Create ctad_01.cpp

---
+++ ders_12/ctad_01.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+int main()
+{
+	using namespace std;
+
+	vector x{ 1, 2, 3 }; // vector<int> x;
+	vector y{ x, x };  //vector<vector<int>> y;
+	vector z{x};  //vector<int> z;
+}
