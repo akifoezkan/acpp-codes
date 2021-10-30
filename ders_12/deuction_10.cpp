7cc5ad48c47a93a96e3407c38da09a6c3e7fdbd9
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-10-30 20:21:16 +0300

    Create deuction_10.cpp

---
+++ ders_12/deuction_10.cpp
@@ -0,0 +1,18 @@
+template <typename T>
+class TypeTeller;
+
+template <typename T>
+void func(T&&)
+{
+	TypeTeller<T> x;
+}
+
+int main()
+{
+	func(5); //T is int 
+	int ival{};
+	func(ival); //T is int &
+	const int cx{20};
+	func(cx);  //T is const int &
+	func("mert"); //T is const char(&)[5]
+}
