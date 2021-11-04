d90fe73e7e20ae82bf231def0042a851001edb88
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-11-04 18:41:17 +0300

    Create if_constexpr_04.cpp

---
+++ ders_15/if_constexpr_04.cpp
@@ -0,0 +1,17 @@
+
+//function return type may be int or double
+
+constexpr auto func()
+{
+	if constexpr (sizeof(int) > 4u) {
+		return 1;
+	}
+	else {
+		return 0.5;
+	}
+}
+
+int main()
+{
+	constexpr auto val = func();
+}
