d47f8117cc217665cd28e7b57be96826171718e0
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-11-05 17:13:26 +0300

    Create sfinae_01.cpp

---
+++ ders_14/sfinae_01.cpp
@@ -0,0 +1,39 @@
+#include <type_traits>
+#include <iterator>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <list>
+
+
+template <typename Iter>
+using IfRandomAccessIter = std::enable_if_t <std::is_same_v<std::random_access_iterator_tag, typename std::iterator_traits<Iter>::iterator_category>>;
+
+template <typename Iter>
+using IfBidirIter = std::enable_if_t <std::is_same_v<std::bidirectional_iterator_tag, typename std::iterator_traits<Iter>::iterator_category>>;
+
+
+template <typename Iter>
+IfRandomAccessIter<Iter> Advance(Iter, int)
+{
+	std::cout << "random access iterator\n";
+}
+
+template <typename Iter>
+IfBidirIter<Iter> Advance(Iter, int)
+{
+	std::cout << "bidirectional iterator\n";
+}
+
+int main()
+{
+	using namespace std;
+
+	vector<int> ivec(10);
+	auto vec_iter = ivec.begin();
+	Advance(vec_iter, 5);
+
+	list<int> ilist(10);
+	auto list_iter = ilist.begin();
+	Advance(list_iter, 5);
+}
