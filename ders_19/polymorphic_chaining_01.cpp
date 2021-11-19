4034c9a6d85cacf32c3d02f3da9265facf07286a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-11-19 10:12:19 +0300

    Create polymorphic_chaining_01.cpp

---
+++ ders_19/polymorphic_chaining_01.cpp
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <ostream>
+
+class Printer {
+public:
+	Printer(std::ostream& os) : m_os{ os } {}
+
+	template <typename T>
+	Printer& print(const T& tval)
+	{
+		m_os << tval;
+		return *this;
+	}
+
+	template <typename T>
+	Printer& print_line(const T& tval)
+	{
+		m_os << tval << '\n';
+		return *this;
+	}
+private:
+	std::ostream& m_os;
+};
+
+#include <iostream>
+
+class ConsolePrinter : public Printer {
+public:
+	ConsolePrinter() : Printer{ std::cout } {}
+
+	ConsolePrinter& set_color(int)
+	{
+		return *this;
+	}
+
+};
+
+int main()
+{
+	ConsolePrinter().print("Kaveh Nematipour").set_color(661).print_line(9763.354);  //invalid
+                                            ----------------
+}
+
