2dbe869dd5bb905d0839f86084375048b16bd045
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2021-11-20 08:10:30 +0300

    Create crtp_print_ntimes.cpp

---
+++ ders_19/crtp_print_ntimes.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+template <typename Derived>
+struct PrintNTimes {
+public:
+	void nprint(std::size_t n)const
+	{
+		while (n--) {
+			static_cast<const Derived&>(*this).print();
+		}
+	}
+};
+
+class Person : public PrintNTimes<Person> {
+public:
+	Person(std::string name, std::string surname) : m_name{ std::move(name) }, m_surname{ std::move(surname) } {}
+	void print()const
+	{
+		std::cout << m_name << " " << m_surname << '\n';
+	}
+private:
+	std::string m_name;
+	std::string m_surname;
+};
+
+int main()
+{
+	Person p{ "Murat", "Hepeyiler" };
+
+	p.nprint(10);
+}
+
+
